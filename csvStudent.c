887bcaa2d6e06950da1a43074696d9bae84b09a9
sierrahowe1 <155486188+sierrahowe1@users.noreply.github.com>
2025-03-02 15:24:52 -0400

    Update csvStudent.c

--- csvStudent.c
+++ csvStudent.c
@@ -6,67 +6,54 @@
 #include "student.h"
 
 void readData(const char *file, Student *student, int *numStudents) {
-   char lines[MAXSIZE];
-   
-   FILE *input = fopen(file, "r");
-   if(input == NULL) {
-      perror("Could not open file");
-      return EXIT_FAILURE;
-   
-   }
-   
-   while(fgets(lines, MAXSIZE, input) != NULL) {
-      Student *stud = &student[*numStudents];
-      char *del = strtok(lines, ",");//stores the student id
-      
-      stud->id = atoi(del);//set the id element in the struct to be the integer equivalent to what is in del currently
-      
-      del = strtok(NULL, ",");//stores the students name
-      strncpy(stud->name, del, MAXLENGTH);//copies the name into the name element of the struct
-      
-      stud->numGrades = 0;
-      while(stud->numGrades < 10) {//checking if the number of grades is less than the max grades allowed
-         stud->grades[stud->numGrades] = atof(del); //changing the grades from ASCII to float and setting the grades element to be equal to the grades at different positions
-         stud->numGrades++; 
-      }
-       
-       
-      stud->GPA = calculateGPA(stud->grades, stud->numGrades);//setting the GPA element of the struct to be a function all to calculateGPA
-       
-      (*numStudents)++;//incrementing the number of students
-      
-   
-   
-   }
-   
-   fclose(input);
-   return EXIT_SUCCESS;
-   
+    char lines[MAXSIZE];
+    
+    FILE *input = fopen(file, "r");
+    if (input == NULL) {
+        perror("Could not open file");
+        return;
+    }
+    
+    while (fgets(lines, MAXSIZE, input) != NULL) {
+        Student *stud = &student[*numStudents];
+        char *del = strtok(lines, ","); // Tokenize the line using comma as delimiter
+        
+        stud->id = atoi(del); // Set the student ID
+        
+        del = strtok(NULL, ","); // Get the student name
+        strncpy(stud->name, del, MAXLENGTH); // Copy the name into the struct
+        
+        stud->numGrades = 0;
+        while ((del = strtok(NULL, ",")) != NULL && stud->numGrades < 10) {
+            stud->grades[stud->numGrades] = atof(del); // Convert grade to float and store it
+            stud->numGrades++; // Increment the number of grades
+        }
+        
+        stud->GPA = calculateGPA(stud->grades, stud->numGrades); // Calculate GPA
+        (*numStudents)++; // Increment the number of students
+    }
+    
+    fclose(input);
 }
 
 void writeData(Student *student, int numStudents) {
-   FILE *output = fopen("studentGrades.txt", "w");
-   if(output == NULL) {
-      perror("Could not open file");
-      return EXIT_FAILURE;
-   }
-   
-   for(int i = 0; i < numStudents; i++) {
-      Student *student1 = &student[i];
-      fprintf(output, "%d,%s", student1->id, student1->name);//printing id and name of the students to the file
-      
-      for(int j = 0; j < student1->numGrades; j++) {
-         fprintf(output, ",%.2f", s->grades[j]);
-      }
-      fprintf("\n");
-   
-   }
-   
-   fclose(output);
-   return EXIT_SUCCESS;
-
-
-
+    FILE *output = fopen("studentGrades.txt", "w");
+    if (output == NULL) {
+        perror("Could not open file");
+        return;
+    }
+    
+    for (int i = 0; i < numStudents; i++) {
+        Student *student1 = &student[i];
+        fprintf(output, "%d,%s", student1->id, student1->name); // Print ID and name
+        
+        for (int j = 0; j < student1->numGrades; j++) {
+            fprintf(output, ",%.2f", student1->grades[j]); // Print grades
+        }
+        fprintf(output, "\n"); // Print a newline after each student
+    }
+    
+    fclose(output);
 }
 
 float calculateGPA(float grades[], int numGrades) {//calculating GPA
@@ -74,7 +61,7 @@ float calculateGPA(float grades[], int numGrades) {//calculating GPA
    float GPA;
    if(numGrades == 0) {
       printf("There are no grades associated with this student");
-      return EXIT_FAILURE;
+      return 0.0;
    }
    
    for(int i = 0; i < numGrades; i++) {
