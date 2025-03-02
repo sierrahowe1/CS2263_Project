642d0e8b546e8ffb8e3d11001ed26d8cff958353
sierrahowe1 <155486188+sierrahowe1@users.noreply.github.com>
2025-03-02 16:17:45 -0400

    Update student.h

--- student.h
+++ student.h
@@ -1,15 +1,17 @@
 //student.h
 #ifndef STUDENT_H
 #define STUDENT_H
-#define MAXSIZE 10
+#define MAXSIZEE 10
 #define MAXLENGTH 1024
 
 typedef struct
 {
-char *name[MAXLENGTH];
+char name[MAXLENGTH];
 int id;
-float grades[MAXSIZE];
+float grades[MAXSIZEE];
+int numGrades;
 float GPA;
 
-}Student
+}Student;
 
+#endif
