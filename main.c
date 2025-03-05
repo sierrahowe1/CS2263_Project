df2c0f77f3f2ae2a47b60703163aa471d1f490ba
sierrahowe1 <155486188+sierrahowe1@users.noreply.github.com>
2025-03-05 11:11:42 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,20 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "student.h"
+#define MAXSTUD 10
 
 void writeData(Student *student, int numStudents);
 void readData(const char *file, Student *student, int *numStudents);
 float calculateGPA(float grades[], int numGrades);
 
 int main() {
-    Student stud[2];
+    Student stud[MAXSTUD];
     int numStudents = 0;
+    
     readData("student.csv", stud, &numStudents);
     
+    stud[0].grades[stud->numGrades+1] = 50.0;
+    
     for(int i = 0; i < numStudents; i++) {
-       printf("%d, %s: \n", stud[i].id, stud[i].name);
+       printf("%d, %s, ", stud[i].id, stud[i].name);
+       
+       
+       
+       for(int j = 0; j < stud->numGrades; j++) {
+          printf("%s, %.1f,", stud[i].classes[j], stud[i].grades[j]);
+       }
+       printf("\n");
        
     }
+    
+    
     
     writeData(stud, numStudents);
 }
