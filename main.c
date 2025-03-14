e5d072921d6b48a077b25bb7e55530463a35b6c5
Juan1ta-C <94313908+Juan1ta-C@users.noreply.github.com>
2025-03-14 12:25:39 -0300

    Update main.c

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 void writeData(Student *student, int numStudents);
 void readData(const char *file, Student *student, int *numStudents);
 float calculateGPA(float grades[], int numGrades);
+void mergeSort(Student arr[], int left, int right, int type);
 
 int main() {
     Student stud[MAXSTUD];
@@ -13,27 +14,18 @@ int main() {
     int type;
     
     readData("student.csv", stud, &numStudents);
-    
-    stud[0].grades[stud->numGrades+1] = 50.0;
-    
+
     for(int i = 0; i < numStudents; i++) {
-       printf("%d, %s, ", stud[i].id, stud[i].name);
-       
-       
-       
-       for(int j = 0; j < stud->numGrades; j++) {
-          printf("%s, %.1f,", stud[i].classes[j], stud[i].grades[j]);
+       printf("%d, %s ", stud[i].id, stud[i].name);
+       for(int j = 0; j < stud[i].numGrades; j++) {
+          printf(", %s, %.1f ", stud[i].classes[j], stud[i].grades[j]);
        }
        printf("\n");
-       
     }
     
-    printf("How do you want to sort: 1.)ID 2.)GPA .3)Name ");
+    printf("How do you want to sort the students:\n 1.) ID\n 2.) GPA\n 3.) Name\n ");
     scanf("%d", &type);
     
-    mergeSort()
-    
-    
-    
+    mergeSort(stud, 0, numStudents-1, type);
     writeData(stud, numStudents);
 }
