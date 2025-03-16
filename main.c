dde0ae1a76855666f04e8e7e667a6fe3c7e2f5d0
sierrahowe1 <155486188+sierrahowe1@users.noreply.github.com>
2025-03-16 16:19:50 -0300

    Update main.c

--- main.c
+++ main.c
@@ -7,25 +7,47 @@ void writeData(Student *student, int numStudents);
 void readData(const char *file, Student *student, int *numStudents);
 float calculateGPA(float grades[], int numGrades);
 void mergeSort(Student arr[], int left, int right, int type);
+void searchForStudent(Student students[], int numStudents);
+Student *searchID(Student arr[], int Id, int numStudents);
+Student *searchName(Student arr[], char *name, int numStudents);
 
 int main() {
     Student stud[MAXSTUD];
     int numStudents = 0;
     int type;
+    int choice;
     
-    readData("student.csv", stud, &numStudents);
-
-    for(int i = 0; i < numStudents; i++) {
-       printf("%d, %s ", stud[i].id, stud[i].name);
-       for(int j = 0; j < stud[i].numGrades; j++) {
-          printf(", %s, %.1f ", stud[i].classes[j], stud[i].grades[j]);
-       }
-       printf("\n");
-    }
+    printf("\n --!Welcome to S&J Student Grade Management System!--\n");
+    printf("\n We offer a variety of services, please check out out menu below \n");
     
-    printf("How do you want to sort the students:\n 1.) ID\n 2.) GPA\n 3.) Name\n ");
-    scanf("%d", &type);
-    
-    mergeSort(stud, 0, numStudents-1, type);
-    writeData(stud, numStudents);
+    do {
+        printf("\nMenu:\n");
+        printf("1. Import student data from CSV\n");
+        printf("2. Sort Students by criteria\n");
+        printf("3. Export students to a CSV with GPA calculated\n");
+        printf("4. Search for a student by name or id\n");
+        printf("5. Exit\n");
+        printf("\nWhat would you like to do today: ");
+        scanf("%d", &choice);
+       
+        if (choice == 1) {
+            readData("student.csv", stud, &numStudents);
+        } else if (choice == 2) {
+            printf("\n --How do you want to sort the students:\n 1.) ID\n 2.) GPA\n 3.) Name\n ");
+            printf("Choice of sort: ");
+            scanf("%d", &type);
+            mergeSort(stud, 0, numStudents-1, type);
+            printf("Sorted!\n");
+        } else if (choice == 3) {
+            writeData(stud, numStudents);
+            printf("Successfully exported students grades with GPA to studentGrades.csv\n");
+        } else if(choice == 4) {
+           printf("Search for student by: \n");
+           searchForStudent(stud, numStudents);
+        }
+        
+    } while (choice != 5);
+     printf("\nThank you for using our system! Have a lovely day\n");
+     printf("\n");
+     return 0;
 }
