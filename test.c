1d548b6a3922c15612670914c0b6608c908a6ecc
Juan1ta-C <94313908+Juan1ta-C@users.noreply.github.com>
2025-03-23 17:45:40 -0300

    Update test.c

--- test.c
+++ test.c
@@ -3,17 +3,20 @@
 #include "student.h"
 #define MAXSTUD 10
 
-void writeData(Student *student, int numStudents);
-void readData(const char *file, Student *student, int *numStudents);
+void writeData(StudentNode** head, int numStudents);
+void readData(const char *file, StudentNode** head, int *numStudents);
 float calculateGPA(float grades[], int numGrades);
-void mergeSort(Student arr[], int left, int right, int type);
-void searchForStudent(Student students[], int numStudents);
-Student *searchID(Student arr[], int Id, int numStudents);
-Student *searchName(Student arr[], char *name, int numStudents);
-void printBarChart(Student student);
-float fmodf(float x, float y);
+void mergeSort(StudentNode** head, int type);
+void searchForStudent(StudentNode **head, int numStudents);
+Student *searchID(StudentNode **head, int Id);
+Student *searchName(StudentNode **head, char *name);
+void addStudent(StudentNode** head, Student newStudent);
+void freeList(StudentNode** head);
+void removeStudent(StudentNode **head, int id);
+void printBarChart(Student *student);
 
 int main() {
+   printf("_______________________________\n");
    //testing GPA
    float invalidGrades[] = {88.0, 101.0, 77.0};
    int size1 = sizeof(invalidGrades)/sizeof(invalidGrades[0]);
@@ -29,38 +32,44 @@ int main() {
    printf("_______________________________\n");
    
    //Empty file reading 
-   Student stud[MAXSTUD];
+   StudentNode* head = NULL;
    int numStudents = 0;
    printf("Empty file test: \n");
-   readData("empty.csv", stud, &numStudents);
+   readData("empty.csv", &head, &numStudents);
    
    printf("_______________________________\n");
 
    //Valid data test
    printf("Valid file test: \n");
-   readData("student.csv", stud, &numStudents);
+   readData("student.csv", &head, &numStudents);
+   
+   
+   printf("_______________________________\n");  
+   
+   
    int type;
    
    
+   StudentNode b ={{4, "Another", {80.0, 75.0}, {"Math","Science"}, 2, 3.3 }, NULL}; 
+   StudentNode a ={{2, "juanita", {99.0, 100.0, 44.0}, {"Math","Science","English"}, 3, 3.5}, &b};
+   StudentNode start = {{1, "sierra", {95.0, 67.0, 55.0}, {"Java","Chemistry","Calculus"}, 3, 3.1}, &a};
    
-   Student student[] = {
-   {1, "sierra", {95.0, 67.0, 55.0}, {"Java","Chemistry","Calculus"}, 3, 3.1},
-   {2, "juanita", {99.0, 100.0, 44.0}, {"Math","Science","English"}, 3, 3.5},
-   {4, "Another", {80.0, 75.0}, {"Math","Science"}, 2, 3.3 }}; 
-   
-   printBarChart(student[0]);
+   Student try = {1, "sierra", {95.0, 67.0, 55.0}, {"Java","Chemistry","Calculus"}, 3, 3.1};
+   Student *pointer = &try;
+   StudentNode* list = &start;
+   printBarChart(pointer);
   
    printf("\n --How do you want to sort the students:\n 1.) ID\n 2.) GPA\n 3.) Name\n ");
             printf("Choice of sort: ");
             scanf("%d", &type);
-            mergeSort(student, 0, numStudents-1, type);
-            for (int i = 0; i < 3; i++) {
-            printf("%d, %s, %.1f", student[i].id, student[i].name, student[i].GPA);
-            
-            for (int j = 0; j < student[i].numGrades; j++) {
-            printf(", %s, %.1f", student[i].classes[j], student[i].grades[j]);
-            
+            mergeSort(&list, type);
+            StudentNode* current = &start;
+            while( current != NULL) {
+            	printf("%d, %s, %.1f", current->data.id, current->data.name, current->data.GPA);
+                for (int j = 0; j < current->data.numGrades; j++) {
+                   printf(", %s, %.1f", current->data.classes[j], current->data.grades[j]);
+                } 
+                printf("\n");
+                current = current->next;
             }
-            printf("\n");
-            
-  }}
+}
