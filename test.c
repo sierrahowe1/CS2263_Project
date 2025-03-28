23b1ccda09262d898175f52591bb9b5549c516db
Juan1ta-C <94313908+Juan1ta-C@users.noreply.github.com>
2025-03-28 20:43:27 -0300

    Update test.c

--- test.c
+++ test.c
@@ -1,60 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "student.h"
-#define MAXSTUD 10
-
-void writeData(StudentNode** head, int numStudents);
-void readData(const char *file, StudentNode** head, int *numStudents);
-float calculateGPA(float grades[], int numGrades);
-void mergeSort(StudentNode** head, int type);
-void searchForStudent(StudentNode **head, int numStudents);
-Student *searchID(StudentNode **head, int Id);
-Student *searchName(StudentNode **head, char *name);
-void addStudent(StudentNode** head, Student newStudent);
-void freeList(StudentNode** head);
-void removeStudent(StudentNode **head, int id);
-void printBarChart(Student *student);
-
-int main() {
-   printf("_______________________________\n");
-   //testing GPA
-   float invalidGrades[] = {88.0, 101.0, 77.0};
-   int size1 = sizeof(invalidGrades)/sizeof(invalidGrades[0]);
-   float validGrades[] = {75.0, 54.0, 67.0, 42.0};
-   int size2 = sizeof(validGrades)/sizeof(validGrades[0]);
-   printf("Invalid Test: \n");
-   printf("%.1f\n", calculateGPA(invalidGrades, size1));
-  
-   printf("\n");
-   printf("Valid Test: \n");
-   printf("%.1f\n", calculateGPA(validGrades, size2));
-   
-   printf("_______________________________\n");
-   
-   //Empty file reading 
-   StudentNode* head = NULL;
-   int numStudents = 0;
-   printf("Empty file test: \n");
-   readData("empty.csv", &head, &numStudents);
-   
-   printf("_______________________________\n");
-
-   //Valid data test
-   printf("Valid file test: \n");
-   readData("student.csv", &head, &numStudents);
-   
-   
-   printf("_______________________________\n");  
-   
-   
-   int type;
-   
-   
-   StudentNode b ={{4, "Another", {80.0, 75.0}, {"Math","Science"}, 2, 3.3 }, NULL}; 
-   StudentNode a ={{2, "juanita", {99.0, 100.0, 44.0}, {"Math","Science","English"}, 3, 3.5}, &b};
-   StudentNode start = {{1, "sierra", {95.0, 67.0, 55.0}, {"Java","Chemistry","Calculus"}, 3, 3.1}, &a};#include <stdio.h>
-#include <stdlib.h>
-#include "student.h"
 
 void writeData(StudentNode** head, int numStudents);
 void readData(const char *file, StudentNode** head, int *numStudents);
@@ -113,7 +59,7 @@ int main() {
 	   student2.numGrades = 3;
 	   student2.grades = malloc(student2.numGrades * sizeof(float));
 	   student2.grades[0] = 95.0;
-	   student2.grades[1] = 67.0;
+	   student2.grades[1] = 7.0;
 	   student2.grades[2] = 55.5;
 	   student2.classes[0] = "Physics";
 	   student2.classes[1] = "Math";
@@ -199,25 +145,17 @@ int main() {
 	     current = current->next;
 	   }
 	   
-   //Testing search	   
-}
-   
-   Student try = {1, "sierra", {95.0, 67.0, 55.0}, {"Java","Chemistry","Calculus"}, 3, 3.1};
-   Student *pointer = &try;
-   StudentNode* list = &start;
-   printBarChart(pointer);
-  
-   printf("\n --How do you want to sort the students:\n 1.) ID\n 2.) GPA\n 3.) Name\n ");
-            printf("Choice of sort: ");
-            scanf("%d", &type);
-            mergeSort(&list, type);
-            StudentNode* current = &start;
-            while( current != NULL) {
-            	printf("%d, %s, %.1f", current->data.id, current->data.name, current->data.GPA);
-                for (int j = 0; j < current->data.numGrades; j++) {
-                   printf(", %s, %.1f", current->data.classes[j], current->data.grades[j]);
-                } 
-                printf("\n");
-                current = current->next;
-            }
+   //Testing search
+   	   //Search for students 
+   	   //searchForStudent(&list, 3);
+   	   
+   //Testing remove
+   	   //Removing invalid student
+   	   int invalidID = 4;
+   	   removeStudent(&head, invalidID); 
+   	 
+	   
+	   //Removing valid student
+   	   int validID = 3;
+   	   removeStudent(&head, validID);   	   	   
 }
